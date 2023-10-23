f23af0e234e89baff46d71590f2fd27832e767b5
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-24 01:31:41 +0200

    feat: adding utils

---
+++ electron/libs.ts
@@ -0,0 +1,24 @@
+import { BrowserWindow } from 'electron';
+import { readFile } from 'node:fs/promises';
+
+export const handleOpenDialog = async (dialog: Electron.Dialog, browserWindow: BrowserWindow) => {
+  const result = await dialog.showOpenDialog(browserWindow, {
+    properties: ['openFile'],
+    filters: [{ name: 'Markdown File', extensions: ['md'] }],
+  });
+  console.log(result);
+
+  if (result.canceled) return;
+
+  const [filePath] = result.filePaths;
+  openFile(browserWindow, filePath);
+
+  return result;
+};
+
+export const openFile = async (browserWindow: BrowserWindow, filePath: string) => {
+  const content = await readFile(filePath, { encoding: 'utf-8' });
+
+  //   sends the selected file content to the renderer process
+  browserWindow.webContents.send('selected-file', content, filePath);
+};
