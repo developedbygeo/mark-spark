6d16743b2d7a67187e84fcefd97f76ad0df6c3a7
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-25 22:01:04 +0200

    feat: preview view works

--- src/components/modules/PreviewView.tsx
+++ src/components/modules/PreviewView.tsx
@@ -1,26 +1,20 @@
-import { useState } from 'react';
 import Markdown from 'markdown-to-jsx';
+
+import { useAppSelector } from 'src/store/hooks';
+import options from 'src/lib/htmlViewParserOpts';
 
 import { Label } from 'src/components/Label';
 import { WithClassName } from 'src/types/UI';
 
-import options from 'src/lib/htmlViewParserOpts';
-import { preprocessHtml } from 'src/lib/data';
-
 const PreviewView = ({ className }: WithClassName) => {
-  const [parsedHtml, setParsedHtml] = useState<null | string>(null);
-
-  window.ipcRenderer.on('selected-file', async (_, content: string, filepath: string) => {
-    const preprocessed = preprocessHtml(content);
-    setParsedHtml(preprocessed);
-  });
+  const previewText = useAppSelector((state) => state.data.preview);
 
   return (
     <section className={className}>
       <h2 className="sr-only">Preview</h2>
       <Label>Preview</Label>
       <div className="h-[80vh] c-scrollbar overflow-y-auto mt-8">
-        {parsedHtml && <Markdown options={options}>{parsedHtml}</Markdown>}
+        {previewText && <Markdown options={options}>{previewText}</Markdown>}
       </div>
     </section>
   );
