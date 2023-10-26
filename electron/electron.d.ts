8afea14d7e4f6ba428ab832a8575340965521af3
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-26 22:04:19 +0200

    chore: declaration file

--- electron/electron.d.ts
+++ electron/electron.d.ts
@@ -3,3 +3,10 @@
 
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
+
+declare interface Window {
+  actionsApi: {
+    showOpenDialog: () => void;
+    onSelectedFile: (callback: (content: string, filePath: string) => void) => void;
+  };
+}
