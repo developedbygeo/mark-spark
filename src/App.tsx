dfe210ad5fa1aa73c841805be316c99e25734315
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-26 22:06:48 +0200

    chore: rendering eventHandler

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Layout from 'src/components/modules/Layout';
 import PreviewView from 'src/components/modules/PreviewView';
 import MarkdownEditor from 'src/components/modules/MarkdownEditor';
+import EventHandler from 'src/components/modules/EventHandler';
 
 const App = () => {
   return (
     <>
+      <EventHandler />
       <Layout>
         <div className="w-full px-6 py-12 grid grid-cols-2 gap-8">
           <MarkdownEditor />
