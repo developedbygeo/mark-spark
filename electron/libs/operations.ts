4b73d89949eacf72c740326cc534c59160203d7c
developedbygeo <georgiosanagnostou@hotmail.com>
2023-10-27 22:31:06 +0200

    feat: adding handleSaveHtmlDialog

--- electron/libs/operations.ts
+++ electron/libs/operations.ts
@@ -1,5 +1,6 @@
-import { BrowserWindow, ipcMain } from 'electron';
+import { BrowserWindow } from 'electron';
 import { readFile } from 'node:fs/promises';
+import { addStyling, exportHtml, saveFile } from '../libs/data-utils';
 
 export const handleOpenDialog = async (dialog: Electron.Dialog, browserWindow: BrowserWindow) => {
   const result = await dialog.showOpenDialog(browserWindow, {
@@ -10,14 +11,32 @@ export const handleOpenDialog = async (dialog: Electron.Dialog, browserWindow: B
   if (result.canceled) return;
 
   const [filePath] = result.filePaths;
-  openFile(browserWindow, filePath);
+  handleOpenFile(browserWindow, filePath);
 
   return result;
 };
 
-export const openFile = async (browserWindow: BrowserWindow, filePath: string) => {
+export const handleOpenFile = async (browserWindow: BrowserWindow, filePath: string) => {
   const content = await readFile(filePath, { encoding: 'utf-8' });
 
-  //   sends the selected file content to the renderer process
   browserWindow.webContents.send('selected-file', content, filePath);
+};
+export const handleSaveHtmlDialog = async (
+  dialog: Electron.Dialog,
+  browserWindow: BrowserWindow,
+  htmlString: string
+) => {
+  const styledHtmlString = await addStyling(htmlString);
+
+  const result = await dialog.showSaveDialog(browserWindow, {
+    title: 'Export as HTML',
+    defaultPath: 'export.html',
+    filters: [{ name: 'HTML', extensions: ['html'] }],
+  });
+
+  const { filePath } = result;
+
+  if (!filePath) return;
+
+  await exportHtml(filePath, styledHtmlString);
 };
